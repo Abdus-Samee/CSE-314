97f06924e5397c89d106055a83b981924f1cfd9c
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2023-01-15 12:16:33 +0600

    Create testticket.c

---
+++ 4. Lottery Scheduling/testticket.c
@@ -0,0 +1,20 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/param.h"
+
+int 
+main(int argc, char *argv[]){
+  //we need only 1 parameter along with the command
+  if(argc != 2){
+    fprintf(2, "Usage: %s sys_call_num command\n", argv[0]);
+    exit(1);
+  }
+
+  if(settickets(atoi(argv[1])) < 0){
+    fprintf(2, "%s: settickets failed\n", argv[0]);
+    exit(1);
+  }
+
+  exit(0);
+}
