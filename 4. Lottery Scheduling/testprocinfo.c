375f5cd1a10ece02348e0901b5bc86b0cf97790f
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2023-01-15 21:00:10 +0600

    Update testprocinfo.c

--- 4. Lottery Scheduling/testprocinfo.c
+++ 4. Lottery Scheduling/testprocinfo.c
@@ -2,7 +2,29 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 #include "kernel/param.h"
+#include "kernel/pstat.h"
 
-int main(){
-    return 0;
+int
+main(int argc, char *argv[]){
+    
+    if(argc > 1){
+        fprintf(2, "Usage: %s sys_call_num command\n", argv[0]);
+        exit(1);
+    }
+
+    struct pstat p;
+
+    if(getpinfo(&p) < 0){
+        fprintf(2, "%s: getpinfo failed\n", argv[0]);
+        exit(1);
+    }
+
+    printf("PID    |    In Use    |    Original Tickets    |    Current Tickets    |    Time Slices\n");
+    for(int i = 0; i < NPROC; i++){
+        if(p.inuse[i]){
+            printf("%d    |    %d    |    %d    |    %d    |    %d\n", p.pid[i], p.inuse[i], p.tickets_original[i], p.tickets_current[i], p.time_slices[i]);
+        }
+    }
+
+    exit(0);
 }
